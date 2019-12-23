4f738765a0c16f9176e893169f6da701486f4098
Simon Waldherr <git@simon.waldherr.eu>
2019-12-23 13:52:02 +0100

    upload all needed files

--- buildForRaspi.sh
+++ buildForRaspi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 env GOOS=linux GOARCH=arm GOARM=5 go build cocktail.go
-scp ./cocktail pi@192.168.178.202:/home/pi/bin/cocktail
+scp cocktail index.html multiplikator.json rezepte.json runCocktail.sh test.html pi@192.168.178.202:/home/pi/bin/
 ssh pi@192.168.178.202 /home/pi/bin/runCocktail.sh
