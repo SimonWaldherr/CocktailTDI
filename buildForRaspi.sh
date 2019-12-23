e1cba2dd55be0f89495220e01dd079d90b4f93c8
Simon Waldherr <git@simon.waldherr.eu>
2019-12-23 13:48:55 +0100

    now you can run ./buildForRaspi.sh on your local machine and it does the rest for you (compile, login, upload, set IO pins, run cocktail binary, logout)

--- buildForRaspi.sh
+++ buildForRaspi.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 env GOOS=linux GOARCH=arm GOARM=5 go build cocktail.go
+scp ./cocktail pi@192.168.178.202:/home/pi/bin/cocktail
+ssh pi@192.168.178.202 /home/pi/bin/runCocktail.sh
---
+++ runCocktail.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo 5 > /sys/class/gpio/unexport;
+/home/pi/bin/cocktail;
