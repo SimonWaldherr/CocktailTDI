427e19cb0b2f1371c967a8b1f94bce85006c7984
Simon Waldherr <git@simon.waldherr.eu>
2019-12-28 18:44:19 +0100

    no comment

--- buildForRaspi.sh
+++ buildForRaspi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 go fmt
 env GOOS=linux GOARCH=arm GOARM=5 go build cocktail.go
+ssh pi@192.168.178.202 /etc/init.d/ctdiinitd stop
 scp cocktail index.html multiplikator.json rezepte.json runCocktail.sh test.html pi@192.168.178.202:/home/pi/bin/
 ssh pi@192.168.178.202 /home/pi/bin/runCocktail.sh
--- runCocktail.sh
+++ runCocktail.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /home/pi/bin/;
 echo 5 > /sys/class/gpio/unexport;
-/home/pi/bin/cocktail;
+#/home/pi/bin/cocktail;
+#ssh pi@192.168.178.202
+/etc/init.d/ctdiinitd restart
