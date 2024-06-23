a2518df181348ab6dca8bfa046ede8130cd3c092
Simon Waldherr <git@simon.waldherr.eu>
2024-06-23 23:49:07 +0200

    no message
    Signed-off-by: Simon Waldherr <git@simon.waldherr.eu>

--- buildForRaspi.sh
+++ buildForRaspi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 go fmt
-env GOOS=linux GOARCH=arm GOARM=5 go build cocktail.go
+env GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-w" cocktail.go
 ssh -t pi@192.168.178.202 sudo /etc/init.d/ctdiinitd stop
 cd ui; npm run build; cd ..
 scp cocktail index.html multiplikator.json rezepte.json runCocktail.sh test.html pi@192.168.178.202:/home/pi/bin/
